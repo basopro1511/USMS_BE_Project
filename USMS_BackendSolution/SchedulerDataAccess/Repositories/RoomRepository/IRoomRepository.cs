407517ab5c2f6372d8787b8582f398515feb1e85
thinh <thinhntce160461@fpt.edu.vn>
2024-11-25 13:13:50 +0700

    RoomService

---
+++ USMS_BackendSolution/SchedulerDataAccess/Repositories/RoomRepository/IRoomRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchedulerDataAccess.Repositories.RoomRepository
+{
+    internal class IRoomRepository
+    {
+    }
+}
---
+++ USMS_BackendSolution/SchedulerService/Controllers/Room/RoomController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SchedulerService.Controllers.Room
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
