6da4cbc5c2db1c87f18af13f0f27e7e9de1d7cf4
basopro1511 <nguyenquochoang2003ct@gmail.com>
2025-01-13 05:10:37 +0700

    done fetch data

---
+++ USMS_BackendSolution/SchedulerService/Repository/SlotRepository/ISlotRepository.cs
@@ -0,0 +1,14 @@
+﻿using SchedulerBusinessObject.ModelDTOs;
+
+namespace SchedulerService.Repository.SlotRepository
+{
+    public interface ISlotRepository
+    {
+        public List<TimeSlotDTO> getAllTimeSlot();
+        public TimeSlotDTO GetTimeSlotById(int id);
+        public bool AddNewTimeSlot(TimeSlotDTO timeSlotDTO);
+        public bool UpdateTimeSlot(TimeSlotDTO timeSlotDTO);
+        public bool ChangeTimeSlotStatus(int slotId, int newStatus);
+
+    }
+}
